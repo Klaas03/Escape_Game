0008b32b4f7e0172b393991da8ab68040c0f0ef1
Klaas4 <79898045+Klaas4@users.noreply.github.com>
2021-10-31 21:00:24 +0100

    initial commit

---
+++ StartRoom.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class StartRoom here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StartRoom extends BaseRoom
+{
+    private int count = 0;
+
+    public StartRoom(EscapeGame game){
+        super(game);
+        setBackground("MapTest.jpg");
+    }
+
+    public void act(){
+
+    }
+}
